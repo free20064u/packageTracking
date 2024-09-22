2e319a4590fc47543f15aafda66a6fc535b7cf57
Alfred <76535763+free20064u@users.noreply.github.com>
2024-09-22 07:11:51 -0700

    workflow added

--- tracker/app/urls.py
+++ tracker/app/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('getaquote/', views.getaquoteView, name='getaquote'),
     path('dashboard/', views.dashboardView, name='dashboard'),
     path('dashboard/addPackage/', views.addPackageView, name='addPackage'),
-    path('dashboard/updatePackage/', views.updatePackageView, name='updatePackage')
+    path('dashboard/updatePackage/', views.updatePackageView, name='updatePackage'),
+    path('updateWebsite/', views.updateWebsiteView, name='updateWebsite'),
     
 ]
